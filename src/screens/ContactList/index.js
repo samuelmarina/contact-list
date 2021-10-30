34aaff60fccb346c124f5cb9143bfb26cd3b605d
samuellmarina <samuellmarina@gmail.com>
2021-10-30 15:40:04 -0600

    Initialize contact list screen

--- src/screens/ContactList/index.js
+++ src/screens/ContactList/index.js
@@ -1,11 +1,50 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { SectionList } from 'react-native';
+
+import Contact from '../../components/Contact';
+import { Container, Separator } from './styles';
+import SectionHeader from '../../components/SectionHeader';
 
 const ContactList = () => {
+  const data = [
+    {
+      title: 'Favs',
+      data: [
+        {
+          name: 'Pedro Perez',
+          isFav: true,
+        },
+      ],
+    },
+    {
+      title: 'Others',
+      data: [
+        {
+          name: 'Pedro Perez',
+          isFav: true,
+        },
+      ],
+    },
+  ];
+
+  const renderItem = ({ item, index }) => {
+    const { name, address, isFav } = item;
+    return <Contact name={name} address={address} isFav={isFav} key={index} />;
+  };
+
+  const renderSectionHeader = ({ section: { title } }) => {
+    return <SectionHeader>{title}</SectionHeader>;
+  };
+
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <Container>
+      <SectionList
+        sections={data}
+        renderItem={renderItem}
+        ItemSeparatorComponent={Separator}
+        renderSectionHeader={renderSectionHeader}
+      />
+    </Container>
   );
 };
 
--- src/screens/ContactList/styles.js
+++ src/screens/ContactList/styles.js
@@ -1,1 +1,11 @@
 import styled from 'styled-components';
+
+export const Container = styled.View({
+  flex: 1,
+});
+
+export const Separator = styled.View({
+  borderBottomWidth: 0.3,
+  borderBottomColor: 'gray',
+  marginHorizontal: 20,
+});
