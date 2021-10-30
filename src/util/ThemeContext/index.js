2f1ff7883be226a696966bb12546c7376359da52
samuellmarina <samuellmarina@gmail.com>
2021-10-30 13:59:27 -0600

    Create theme manager

---
+++ src/util/ThemeContext/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+export const ThemeContext = React.createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme(prev => (prev === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
