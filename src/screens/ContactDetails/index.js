ebc752479f7d9ab0166d3c90ae3886b23cfc0d73
samuellmarina <samuellmarina@gmail.com>
2021-10-30 16:47:12 -0600

    Initialize contact details screen

--- src/screens/ContactDetails/index.js
+++ src/screens/ContactDetails/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  Container,
+  HeaderContainer,
+  ContactImage,
+  Title,
+  Subtitle,
+  Separator,
+} from './styles';
+
+import DetailCard from '../../components/DetailCard';
 
 const ContactDetails = () => {
+  const src = '../../assets/user/large/user-lg.png';
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <Container>
+      <HeaderContainer>
+        <ContactImage source={require(src)} size={150} />
+        <Title>Pedro Perez</Title>
+        <Subtitle>Calle La Cantera</Subtitle>
+      </HeaderContainer>
+      <Separator />
+      <DetailCard label="Phone" value="(302) 559-0519" type="Mobile" />
+    </Container>
   );
 };
 
--- src/screens/ContactDetails/styles.js
+++ src/screens/ContactDetails/styles.js
@@ -1,1 +1,38 @@
 import styled from 'styled-components';
+
+export const Container = styled.ScrollView({
+  flex: 1,
+  padding: 20,
+  backgroundColor: 'white',
+});
+
+export const HeaderContainer = styled.View({
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: 10,
+  marginBottom: 15,
+});
+
+export const ContactImage = styled.Image(({ size = 100 }) => ({
+  width: size,
+  height: size,
+  marginBottom: 15,
+}));
+
+export const Title = styled.Text({
+  color: 'black',
+  fontSize: 30,
+  marginBottom: 5,
+});
+
+export const Subtitle = styled.Text({
+  color: 'gray',
+  fontSize: 15,
+});
+
+export const Separator = styled.View({
+  borderBottomWidth: 0.3,
+  borderBottomColor: 'gray',
+  width: '100%',
+  marginVertical: 10,
+});
