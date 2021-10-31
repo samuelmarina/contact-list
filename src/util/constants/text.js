456e00cf9cb1bd32c381bba3f75bed52d38ee5f9
samuellmarina <samuellmarina@gmail.com>
2021-10-31 12:44:46 -0600

    Add text constants

---
+++ src/util/constants/text.js
@@ -0,0 +1,8 @@
+export const text = {
+  emailAddress: 'Email',
+  phone: 'Phone',
+  address: 'Address',
+  birthdate: 'Birthdate',
+  favContacts: 'Favorite Contacts',
+  otherContacts: 'Other Contacts',
+};
