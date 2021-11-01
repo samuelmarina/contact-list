db04551128937f57121c6d270c8f0d8167fdbda8
samuellmarina <samuellmarina@gmail.com>
2021-10-31 23:40:16 -0600

    Change color

--- src/components/SectionHeader/styles.js
+++ src/components/SectionHeader/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.View({
   width: '100%',
-  backgroundColor: 'gray',
+  backgroundColor: '#e1e1e1',
   padding: 5,
   paddingLeft: 15,
 });
