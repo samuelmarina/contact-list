cf75ba383662d5197586a70162a0b29e05163693
samuellmarina <samuellmarina@gmail.com>
2021-10-31 21:47:42 -0600

    Create new helper

---
+++ src/helpers/contacts/index.js
@@ -0,0 +1,38 @@
+import { nameComparator } from '../comparators';
+import { text } from '../../util/constants/text';
+
+export const addToFavorites = (
+  data,
+  index,
+  favList,
+  othersList,
+  setFavList,
+  setOthersList,
+) => {
+  const othersArr = othersList.data;
+  othersArr.splice(index, 1);
+  const favArr = favList.data;
+  data.isFavorite = true;
+  favArr.push(data);
+  favArr.sort(nameComparator);
+  setFavList({ title: text.favContacts, data: favArr });
+  setOthersList({ title: text.otherContacts, data: othersArr });
+};
+
+export const removeFromFavorites = (
+  data,
+  index,
+  favList,
+  othersList,
+  setFavList,
+  setOthersList,
+) => {
+  const favArr = favList.data;
+  favArr.splice(index, 1);
+  const othersArr = othersList.data;
+  data.isFavorite = false;
+  othersArr.push(data);
+  othersArr.sort(nameComparator);
+  setFavList({ title: text.favContacts, data: favArr });
+  setOthersList({ title: text.otherContacts, data: othersArr });
+};
