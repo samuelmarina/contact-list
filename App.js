315e2d6677b1d2180c73681887b257ee27a55175
samuellmarina <samuellmarina@gmail.com>
2021-11-02 13:04:10 -0600

    Minor fix

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
-
 import { ThemeProvider } from './src/util/ThemeContext';
 import AppNavigator from './src/navigation/AppNavigator';
 
